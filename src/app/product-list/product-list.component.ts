42efae6d3a8f7fc02c7af36d57da8c1fbc8f2eaf
Marek <marek@switajski.de>
2019-12-06 15:29:11 +0100

    Add adding of products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 
-interface Product { name: string, price: number }
+interface Product { name: string, price?: number }
 
 @Component({
   selector: 'app-product-list',
   template: `<h2>Products</h2>
   <ul>
-    <li *ngFor="let product of products">{{product.name}}, at {{product.price}}</li>
-  </ul>`,
+    <li *ngFor="let product of products">{{product.name | uppercase}}, at {{product.price}}</li>
+  </ul>
+  <label>name:
+    <input [(ngModel)]="product.name" placeholder="name"/>
+  </label>
+  <button (click)="addSome()" >add</button>`,
 })
 export class ProductListComponent {
   products: Product[] = [
     { name: 'apple', price: 0.5 },
     { name: 'banana', price: 0.5 },
     { name: 'clementine', price: 0.7 }];
+  product: Product = {name:''}
+
+  addSome(){
+    this.products.push(this.product)
+  }
+  addEdited(){
+    this.products.push(this.product)
+  }
 }
